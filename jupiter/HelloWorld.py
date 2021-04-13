edae1abe67bee83d260f5c4d05e51458aac6297e
bjorn chrisnach <bjorn.chrisnach@gmail.com>
2021-04-13 07:06:33 +0200

    Readme_gitignore_update

---
+++ jupiter/HelloWorld.py
@@ -0,0 +1,24 @@
+# def print_Kevin(text):
+#    print(text)
+#   print(text)
+#    print(text)
+
+
+# print_Kevin("Kevin Stratvert has a great channel")
+# def school_age_calculator(age, name):
+#    if age < 5:
+#        print("Enjoy the time!", name, "is only", age)
+#    elif age == 5:
+#        print("Enjoy Kindergarten,", name)
+#    else:
+#        print("they grow up so fast!")
+
+# school_age_calculator(5, "Thomas")
+
+def add_ten_to_age(age):
+    new_age = age + 10
+    return new_age
+
+
+How_Old_Will_I_Be = add_ten_to_age(3)
+print(How_Old_Will_I_Be)
